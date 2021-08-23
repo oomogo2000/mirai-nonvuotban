5a3e9d9eb985b906f94577c334d80f8c29c2abac
oomogo2000 <89309405+oomogo2000@users.noreply.github.com>
2021-08-23 18:36:27 +0700

    Add files via upload

--- modules/commands/loli.js
+++ modules/commands/loli.js
@@ -13,7 +13,7 @@ module.exports.run = async ({ api, event }) => {
 	const axios = require('axios');
 	const request = require('request');
 	const fs = require("fs");
-	axios.get('https://www.api-adreno.tk/loli').then(res => {
+	axios.get('https://api.nekos.dev/api/v3/images/nsfw/img/smallboobs_lewd/').then(res => {
 	let ext = res.data.data.response.url.substring(res.data.data.response.url.lastIndexOf(".") + 1);
 	let callback = function () {
 					api.sendMessage({
