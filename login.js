d2a7da1b21571aa6978f90eb5d7dca2ec518cd61
Kyouka-kute <89309405+oomogo2000@users.noreply.github.com>
2022-03-22 14:32:07 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,22 +1,28 @@
-//const chrome = require('chrome-cookies-secure');
-//var fs = require('fs');
+/*Để có thể sử dụng được bản đang bị comments yêu cầu:
+- Máy đã tải và sử dụng Python2
+- Vì là lấy cookies trên chrome nên replit không thể sử dụng được
+- Khi sử dụng bản 1 yêu cầu comments lại bản 2 */
 
-//chrome.getCookies('https://www.facebook.com/', 'puppeteer', function(err, cookies) {
-  //  let cooky = cookies.map(mapper => ({
-    //    key: mapper.name,
-      //  value: mapper.value,
-        //domain: "facebook.com",
-        //path: mapper.path,
-        //hostOnly: mapper.hostOnly,
-        //creation: new Date().toISOString(),
-    //    lastAccessed: new Date().toISOString()
-    //}));
-    //let appstate = JSON.stringify(cooky, null, 4);
-    //fs.writeFile('appstate.json', appstate, 'utf8', function(err) {
-      //  if (err) throw err;
-       // console.log('complete');
-    //});
-//});
+/*
+const chrome = require('chrome-cookies-secure');
+var fs = require('fs');
+chrome.getCookies('https://www.facebook.com/', 'puppeteer', function(err, cookies) {
+    let cooky = cookies.map(mapper => ({
+        key: mapper.name,
+        value: mapper.value,
+        domain: "facebook.com",
+        path: mapper.path,
+        hostOnly: mapper.hostOnly,
+        creation: new Date().toISOString(),
+        lastAccessed: new Date().toISOString()
+    }));
+    let appstate = JSON.stringify(cooky, null, 4);
+    fs.writeFile('appstate.json', appstate, 'utf8', function(err) {
+        if (err) throw err;
+        console.log('complete');
+    });
+});
+*/
 const fs = require("fs-extra");
 const login = require("sieudangyeu");
 const readline = require("readline");
